9aa62abcfc568e586074a7b1a47242076af22cd3
Ryukishi <donnyschaafs@gmail.com>
2019-12-09 14:19:50 +0100

    Character occurence
    added functionality

--- src/main/java/nl/hva/ict/se/sands/HuffmanCompression.java
+++ src/main/java/nl/hva/ict/se/sands/HuffmanCompression.java
@@ -1,9 +1,13 @@
 package nl.hva.ict.se.sands;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+ * Original Huffman Compression algorithm taken from Huffman.java in Algorithms 4th edition.
+ */
 public class HuffmanCompression {
     private final String text;
 
@@ -49,6 +53,42 @@ public class HuffmanCompression {
      */
     Map<Character, String> getCodes() {
         return null;
+    }
+
+    private HashMap charOccurrence() {
+
+        // Creating a HashMap containing char
+        // as a key and occurrences as  a value
+        HashMap<Character, Integer> charCountMap
+                = new HashMap<Character, Integer>();
+
+        // Converting given string to char array
+
+        char[] strArray = text.toCharArray();
+
+        // checking each char of strArray
+        for (char c : strArray) {
+            if (charCountMap.containsKey(c)) {
+
+                // If char is present in charCountMap,
+                // incrementing it's count by 1
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            }
+            else {
+
+                // If char is not present in charCountMap,
+                // putting this char to charCountMap with 1 as it's value
+                charCountMap.put(c, 1);
+            }
+        }
+
+        // Printing the charCountMap
+//        for (Map.Entry entry : charCountMap.entrySet()) {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
+
+        return charCountMap;
+
     }
 
 }
